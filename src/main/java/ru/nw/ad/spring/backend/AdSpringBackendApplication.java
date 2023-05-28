fbeeed0b9cf24ebbf01882edce5052a171b012af
Nikita Belonogov <crcku.mark00@gmail.com>
2023-05-29 02:35:15 +0300

    created project and add some dependencies

---
+++ src/main/java/ru/nw/ad/spring/backend/AdSpringBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.nw.ad.spring.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdSpringBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdSpringBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/nw/ad/spring/backend/AdSpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nw.ad.spring.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdSpringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
